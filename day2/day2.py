f2b126c9fff618ae37c2866e8c3ca61b259f7e3a
Axel Larsson <axoxelol_01@hotmail.com>
2024-12-04 00:31:40 +0100

    Lower complexity solutoin for d2p2, much uglier

--- day2/day2.py
+++ day2/day2.py
@@ -13,12 +13,31 @@ def safe(report: list[int]) -> bool:
     return True
 
 
+# def safe_p2(report: list[int]) -> bool:
+#     for i in range(len(report)):
+#         rep = report[:i] + report[i + 1 :]
+#         if safe(rep):
+#             return True
+#     return False
+
+
 def safe_p2(report: list[int]) -> bool:
-    for i in range(len(report)):
-        rep = report[:i] + report[i + 1 :]
-        if safe(rep):
-            return True
-    return False
+    for i in range(len(report) - 1):
+        diff = report[i + 1] - report[i]
+        if not (1 <= abs(diff) <= 3):
+            return safe(report[:i] + report[i + 1 :]) or safe(
+                report[: i + 1] + report[i + 2 :]
+            )
+        if (not (i + 2 >= len(report))) and (
+            (report[i + 1] - report[i]) * (report[i + 2] - report[i + 1]) < 0
+        ):
+            return (
+                safe(report[:i] + report[i + 1 :])
+                or safe(report[: i + 1] + report[i + 2 :])
+                or safe(report[: i + 2] + report[i + 3 :])
+            )
+
+    return safe(report)
 
 
 sol1 = sum(map(safe, reports))
